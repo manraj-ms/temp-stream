bb5b4b1b23627ea21287ba5401a7f5fc51c8fd94
Manraj Singh <contactmanraj2001@gmail.com>
2025-04-28 18:35:43 +0530

    .

--- services/stream.service.js
+++ services/stream.service.js
@@ -1,15 +1,15 @@
-import TwinProtocol from "twin-protocol-staging";
+import TwinProtocol from "twin-protocol-prod";
 import dotenv from "dotenv";
 dotenv.config();
 
 class StreamService {
   constructor() {
     this.twinProtocol = new TwinProtocol({
-      accessKey: process.env.TP_ACCESS_KEY,
-      secretKey: process.env.TP_SECRET_KEY,
-      clientId: process.env.TP_CLIENT_ID,
-      baseUrl: process.env.TP_BASE_URL,
-      wsUrl: process.env.TP_WS_URL
+      TP_ACCESS_KEY: process.env.TP_ACCESS_KEY,
+      TP_SECRET_KEY: process.env.TP_SECRET_KEY,
+      TP_CLIENT_ID: process.env.TP_CLIENT_ID,
+      TP_BASE_URL: process.env.TP_BASE_URL,
+      TP_WS_URL: process.env.TP_WS_URL,
     });
   }
 
@@ -17,42 +17,42 @@ class StreamService {
     console.log("hello")
     stitch = true
     session_timeout = 300
-    try{
-        const response = await this.twinProtocol.setupConnection(twinId, responseResolution, stitch, session_timeout)
-        console.log(response, 19)
-        return response
+    try {
+      const response = await this.twinProtocol.setupConnection(twinId, responseResolution, stitch, session_timeout)
+      console.log(response, 19)
+      return response
     }
-    catch(error){
+    catch (error) {
       throw error
     }
   }
 
   async startStream(streamId, input, twinId, sessionId) {
-    try{
-        const response = await this.twinProtocol.startStream(streamId, input, twinId, sessionId)
-        return response
+    try {
+      const response = await this.twinProtocol.startStream(streamId, input, twinId, sessionId)
+      return response
     }
-    catch(error){
-        throw error
+    catch (error) {
+      throw error
     }
   }
 
   async startConnection(streamId, answer, sessionId) {
-    try{
-        const response = await this.twinProtocol.startConnection(streamId, answer, sessionId)
-        return response
+    try {
+      const response = await this.twinProtocol.startConnection(streamId, answer, sessionId)
+      return response
     }
-    catch(error){
+    catch (error) {
       throw error
     }
   }
 
   async sendNetworkInfo(streamId, sessionId, candidate, sdpMid, sdpMLineIndex) {
-    try{
-        const response = await this.twinProtocol.sendNetworkInfo(streamId, sessionId, candidate, sdpMid, sdpMLineIndex)
-        return response
+    try {
+      const response = await this.twinProtocol.sendNetworkInfo(streamId, sessionId, candidate, sdpMid, sdpMLineIndex)
+      return response
     }
-    catch(error){
+    catch (error) {
       throw error
     }
   }
