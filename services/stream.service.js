5afbd4dee152d899b3deae9c7147c9f7e0b4017d
Manraj Singh <contactmanraj2001@gmail.com>
2025-05-15 18:25:14 +0530

    .

--- services/stream.service.js
+++ services/stream.service.js
@@ -1,20 +1,20 @@
-import TwinProtocol from "twin-protocol-prod";
+import TwinProtocol from "twin-protocol-staging";
 import dotenv from "dotenv";
 dotenv.config();
 
 class StreamService {
   constructor() {
     this.twinProtocol = new TwinProtocol({
-      TP_ACCESS_KEY: process.env.TP_ACCESS_KEY,
-      TP_SECRET_KEY: process.env.TP_SECRET_KEY,
-      TP_CLIENT_ID: process.env.TP_CLIENT_ID,
-      TP_BASE_URL: process.env.TP_BASE_URL,
-      TP_WS_URL: process.env.TP_WS_URL,
-      // accessKey: process.env.TP_ACCESS_KEY,
-      // secretKey: process.env.TP_SECRET_KEY,
-      // clientId: process.env.TP_CLIENT_ID,
-      // baseUrl: process.env.TP_BASE_URL,
-      // wsUrl: process.env.TP_WS_URL,
+      // TP_ACCESS_KEY: process.env.TP_ACCESS_KEY,
+      // TP_SECRET_KEY: process.env.TP_SECRET_KEY,
+      // TP_CLIENT_ID: process.env.TP_CLIENT_ID,
+      // TP_BASE_URL: process.env.TP_BASE_URL,
+      // TP_WS_URL: process.env.TP_WS_URL,
+      accessKey: process.env.TP_ACCESS_KEY,
+      secretKey: process.env.TP_SECRET_KEY,
+      clientId: process.env.TP_CLIENT_ID,
+      baseUrl: process.env.TP_BASE_URL,
+      wsUrl: process.env.TP_WS_URL,
     });
   }
 
